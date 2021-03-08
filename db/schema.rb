a64d42c1f27d663f42b3c2145f74b19d3786e358
matheusmarquess03 <matheusmarques03@yahoo.com>
2021-03-08 17:33:47 -0300

    creating heroes

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_03_08_192529) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "heroes", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
